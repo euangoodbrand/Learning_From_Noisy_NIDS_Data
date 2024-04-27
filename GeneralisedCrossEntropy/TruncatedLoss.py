a0926960dbc78a944c16c888db992dcb32a61063
Euan Goodbrand <euangoodbrand@aol.com>
2024-04-27 20:23:48 +0100

    Added generalised cross entropy folder, implementation and bash script of experiments
    Added generalised cross entropy folder, implementation and bash script of experiments

---
+++ GeneralisedCrossEntropy/TruncatedLoss.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import numpy as np
+
+# from https://github.com/AlanChou/Truncated-Loss/tree/master
+
+class TruncatedLoss(nn.Module):
+
+    def __init__(self, q=0.7, k=0.5, trainset_size=50000):
+        super(TruncatedLoss, self).__init__()
+        self.q = q
+        self.k = k
+        self.weight = torch.nn.Parameter(data=torch.ones(trainset_size, 1), requires_grad=False)
+             
+    def forward(self, logits, targets, indexes):
+        logits = torch.clamp(logits, min=-10, max=10)
+
+        p = F.softmax(logits, dim=1)
+        Yg = torch.gather(p, 1, torch.unsqueeze(targets, 1))
+
+        loss = ((1-(Yg**self.q))/self.q)*self.weight[indexes] - ((1-(self.k**self.q))/self.q)*self.weight[indexes]
+        loss = torch.mean(loss)
+
+        return loss
+
+    def update_weight(self, logits, targets, indexes):
+        p = F.softmax(logits, dim=1)
+        # epsilon = 1e-8
+        Yg = torch.gather(p, 1, torch.unsqueeze(targets, 1))
+        # Yg = torch.clamp(Yg, min=epsilon, max=1-epsilon)
+        Lq = ((1-(Yg**self.q))/self.q)
+        Lqk = np.repeat(((1-(self.k**self.q))/self.q), targets.size(0))
+        Lqk = torch.from_numpy(Lqk).type(torch.cuda.FloatTensor)
+        Lqk = torch.unsqueeze(Lqk, 1)
+        
+
+        condition = torch.gt(Lqk, Lq)
+        self.weight[indexes] = condition.type(torch.cuda.FloatTensor)
+
+        
