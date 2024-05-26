18c4c843c23e4f2ba4244375fd0c80a301850aa2
Euan Goodbrand <70519638+euangoodbrand@users.noreply.github.com>
2024-05-26 02:30:35 +0100

    Feature noise technique's bash script fixes for experiments 1-3
    Feature noise technique's bash script fixes for experiments 1-3

--- FeatureNoiseLearning/WeightDecay/weightDecay.sh
+++ FeatureNoiseLearning/WeightDecay/weightDecay.sh
@@ -11,10 +11,20 @@ for seed in 1 #2 3 4 5
 do
   for model_type in weightDecay
   do
-    # Experiment 1: Feature Noise (Additive and Multiplicative)
+    # Experiment 1: Feature Noise (Additive)
+    for add_noise_level in "${feature_add_noise_levels[@]}"; do
+        CUDA_LAUNCH_BLOCKING=1 python weightDecay.py --dataset BODMAS --model_type ${model_type} --data_augmentation none --feature_add_noise_level ${add_noise_level} --imbalance_ratio 0 --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_1$
+    done
+
+    # Experiment 2: Feature Noise (Multiplicative)
+    for mult_noise_level in "${feature_mult_noise_levels[@]}"; do
+        CUDA_LAUNCH_BLOCKING=1 python weightDecay.py --dataset BODMAS --model_type ${model_type} --data_augmentation none --feature_mult_noise_level ${mult_noise_level} --imbalance_ratio 0 --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_2$
+    done
+
+    # Experiment 3: Feature Noise (Additive and Multiplicative)
     for add_noise_level in "${feature_add_noise_levels[@]}"; do
         for mult_noise_level in "${feature_mult_noise_levels[@]}"; do
-            CUDA_LAUNCH_BLOCKING=1 python weightDecay.py --dataset BODMAS --model_type ${model_type} --data_augmentation none --feature_add_noise_level ${add_noise_level} --feature_mult_noise_level ${mult_noise_level} --imbalance_ratio 0 --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_1$
+            CUDA_LAUNCH_BLOCKING=1 python weightDecay.py --dataset BODMAS --model_type ${model_type} --data_augmentation none --feature_add_noise_level ${add_noise_level} --feature_mult_noise_level ${mult_noise_level} --imbalance_ratio 0 --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_3$
         done
     done
 
