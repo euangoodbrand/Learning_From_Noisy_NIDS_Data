a3ece99c1d9acbacbfedc2b0e1d551756c6c1f70
Euan Goodbrand <euangoodbrand@aol.com>
2024-06-11 10:43:49 +0100

    Added l2 regularisation and pipeline fixes to label elr code
    Added l2 regularisation and pipeline fixes to label elr code

--- LabelNoiseLearning/EarlyLearningRegularisation/earlyLearningRegularisation.py
+++ LabelNoiseLearning/EarlyLearningRegularisation/earlyLearningRegularisation.py
@@ -78,6 +78,7 @@ parser.add_argument('--imbalance_ratio', type=float, default=0.0, help='Ratio to
 parser.add_argument('--weight_resampling', type=str, choices=['none','Naive', 'Focal', 'Class-Balance'], default='none', help='Select the weight resampling method if needed')
 parser.add_argument('--feature_add_noise_level', type=float, default=0.0, help='Level of additive noise for features')
 parser.add_argument('--feature_mult_noise_level', type=float, default=0.0, help='Level of multiplicative noise for features')
+parser.add_argument('--weight_decay', type=float, default=0.0, help='Weight decay for L2 regularization. Default is 0 (no regularization).')
 
 args = parser.parse_args()
 
@@ -441,7 +442,7 @@ save_dir = args.result_dir +'/' +args.dataset+'/%s/' % args.model_type
 if not os.path.exists(save_dir):
     os.system('mkdir -p %s' % save_dir)
 
-model_str = f"{args.model_type}_{args.dataset}_{'no_augmentation' if args.data_augmentation == 'none' else args.data_augmentation}_{args.noise_type}-noise{args.noise_rate}_imbalance{args.imbalance_ratio}_addNoise{args.feature_add_noise_level}_multNoise{args.feature_mult_noise_level}"
+model_str = f"{args.model_type}_{args.dataset}_{'no_augmentation' if args.data_augmentation == 'none' else args.data_augmentation}_{args.noise_type}-noise{args.noise_rate}_imbalance{args.imbalance_ratio}_addNoise{args.feature_add_noise_level}_multNoise{args.feature_mult_noise_level}_L2_{args.weight_decay}"
 
 txtfile = save_dir + "/" + model_str + ".csv"
 nowTime = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
@@ -844,7 +845,7 @@ def main():
 
         model = MLPNet(num_features=X_train_fold.shape[1], num_classes=len(np.unique(y_train_fold)), dataset=args.dataset).cuda()
         model.apply(weights_init)
-        optimizer = optim.Adam(model.parameters(), lr=args.lr)
+        optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
         # Initialization of elr_loss with parameters
         num_examples = len(X_train_fold)  
@@ -875,7 +876,7 @@ def main():
     # Prepare the full model
     full_model = MLPNet(num_features=X_train_augmented.shape[1], num_classes=len(np.unique(y_train_augmented)), dataset=args.dataset).cuda()
     full_model.apply(weights_init)
-    full_optimizer = optim.Adam(full_model.parameters(), lr=args.lr)
+    full_optimizer = optim.Adam(full_model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
     # Assuming you've calculated the total number of examples and the number of classes
     num_examples = len(y_train_augmented)  # Adjust based on the current training dataset size
     num_classes = len(np.unique(y_train_augmented))  # Dynamically determine the number of unique classes
--- LabelNoiseLearning/EarlyLearningRegularisation/elr.sh
+++ LabelNoiseLearning/EarlyLearningRegularisation/elr.sh
@@ -73,19 +73,19 @@ for seed in 1; do #2 3 4 5
         # New Experiments for feature noise
         # Experiment 9: Additive noise levels
         for add_noise_level in "${feature_add_noise_levels[@]}"; do
-            CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset BODMAS --model_type ${model_type} --data_augmentation none --noise_rate 0  --imbalance_ratio 0 --feature_add_noise_level ${add_noise_level} --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_9$
+            CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset windows_pe_real --model_type ${model_type} --data_augmentation none --noise_rate 0  --imbalance_ratio 0 --feature_add_noise_level ${add_noise_level} --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_9$
         done
 
         # Experiment 10: Multiplicative noise levels
         for mult_noise_level in "${feature_mult_noise_levels[@]}"; do
-            CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset BODMAS --model_type ${model_type} --data_augmentation none --noise_rate 0  --imbalance_ratio 0 --feature_mult_noise_level ${mult_noise_level} --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_10$
+            CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset windows_pe_real --model_type ${model_type} --data_augmentation none --noise_rate 0  --imbalance_ratio 0 --feature_mult_noise_level ${mult_noise_level} --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_10$
         done
 
         # Experiment 11: Combination of additive and multiplicative noise
         for add_noise_level in "${feature_add_noise_levels[@]}"; do
             for mult_noise_level in "${feature_mult_noise_levels[@]}"; do
                 if [[ "${add_noise_level}" != "0.0" || "${mult_noise_level}" != "0.0" ]]; then
-                    CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset BODMAS --model_type ${model_type} --data_augmentation none --noise_rate 0  --imbalance_ratio 0 --feature_add_noise_level ${add_noise_level} --feature_mult_noise_level ${mult_noise_level} --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_11$
+                    CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset windows_pe_real --model_type ${model_type} --data_augmentation none --noise_rate 0  --imbalance_ratio 0 --feature_add_noise_level ${add_noise_level} --feature_mult_noise_level ${mult_noise_level} --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_11$
                 fi
             done
         done
@@ -93,14 +93,48 @@ for seed in 1; do #2 3 4 5
         # Experiment 12: Combination of label noise with additive noise
         for noise_rate in "${noise_rates[@]}"; do
             for add_noise_level in "${feature_add_noise_levels[@]}"; do
-                CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset BODMAS --model_type ${model_type} --data_augmentation none --noise_rate ${noise_rate} --noise_type uniform --imbalance_ratio 0 --feature_add_noise_level ${add_noise_level} --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_12$
+                CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset windows_pe_real --model_type ${model_type} --data_augmentation none --noise_rate ${noise_rate} --noise_type uniform --imbalance_ratio 0 --feature_add_noise_level ${add_noise_level} --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_12$
             done
         done
 
         # Experiment 13: Combination of label noise with multiplicative noise
         for noise_rate in "${noise_rates[@]}"; do
             for mult_noise_level in "${feature_mult_noise_levels[@]}"; do
-                CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset BODMAS --model_type ${model_type} --data_augmentation none --noise_rate ${noise_rate} --noise_type uniform --imbalance_ratio 0 --feature_mult_noise_level ${mult_noise_level} --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_13$
+                CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset windows_pe_real --model_type ${model_type} --data_augmentation none --noise_rate ${noise_rate} --noise_type uniform --imbalance_ratio 0 --feature_mult_noise_level ${mult_noise_level} --seed ${seed} --num_workers ${num_workers} --result_dir results/experiment_13$
+            done
+        done
+
+        # New Experiments with L2 Regularization
+        # Experiment 14: Additive noise levels with L2 regularization
+        for add_noise_level in "${feature_add_noise_levels[@]}"; do
+            CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset windows_pe_real --model_type ${model_type} --data_augmentation none --noise_rate 0  --imbalance_ratio 0 --feature_add_noise_level ${add_noise_level} --seed ${seed} --num_workers ${num_workers} --weight_decay 0.01 --result_dir results/experiment_14$
+        done
+
+        # Experiment 15: Multiplicative noise levels with L2 regularization
+        for mult_noise_level in "${feature_mult_noise_levels[@]}"; do
+            CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset windows_pe_real --model_type ${model_type} --data_augmentation none --noise_rate 0  --imbalance_ratio 0 --feature_mult_noise_level ${mult_noise_level} --seed ${seed} --num_workers ${num_workers} --weight_decay 0.01 --result_dir results/experiment_15$
+        done
+
+        # Experiment 16: Combination of additive and multiplicative noise with L2 regularization
+        for add_noise_level in "${feature_add_noise_levels[@]}"; do
+            for mult_noise_level in "${feature_mult_noise_levels[@]}"; do
+                if [[ "${add_noise_level}" != "0.0" || "${mult_noise_level}" != "0.0" ]]; then
+                    CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset windows_pe_real --model_type ${model_type} --data_augmentation none --noise_rate 0  --imbalance_ratio 0 --feature_add_noise_level ${add_noise_level} --feature_mult_noise_level ${mult_noise_level} --seed ${seed} --num_workers ${num_workers} --weight_decay 0.01 --result_dir results/experiment_16$
+                fi
+            done
+        done
+
+        # Experiment 17: Combination of label noise with additive noise and L2 regularization
+        for noise_rate in "${noise_rates[@]}"; do
+            for add_noise_level in "${feature_add_noise_levels[@]}"; do
+                CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset windows_pe_real --model_type ${model_type} --data_augmentation none --noise_rate ${noise_rate} --noise_type uniform --imbalance_ratio 0 --feature_add_noise_level ${add_noise_level} --seed ${seed} --num_workers ${num_workers} --weight_decay 0.01 --result_dir results/experiment_17$
+            done
+        done
+
+        # Experiment 18: Combination of label noise with multiplicative noise and L2 regularization
+        for noise_rate in "${noise_rates[@]}"; do
+            for mult_noise_level in "${feature_mult_noise_levels[@]}"; do
+                CUDA_LAUNCH_BLOCKING=1 python earlyLearningRegularisation.py --dataset windows_pe_real --model_type ${model_type} --data_augmentation none --noise_rate ${noise_rate} --noise_type uniform --imbalance_ratio 0 --feature_mult_noise_level ${mult_noise_level} --seed ${seed} --num_workers ${num_workers} --weight_decay 0.01 --result_dir results/experiment_18$
             done
         done
 
